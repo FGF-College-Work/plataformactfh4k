e939cc5db505541c85be14c61c85b615fa9b5b40
Boot <h4kboot@gmail.com>
2016-07-09 10:03:32 -0300

    Atualização

--- bd/sucurihc_ctf.sql
+++ bd/sucurihc_ctf.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.0
--- http://www.phpmyadmin.net
+-- version 4.6.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: mysql.sucurihc.org
--- Generation Time: Apr 29, 2016 at 01:43 AM
--- Server version: 5.6.25-log
--- PHP Version: 7.0.4
+-- Tempo de geração: 09/07/2016 às 06:02
+-- Versão do servidor: 5.6.25-log
+-- Versão do PHP: 7.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sucurihc_ctf`
+-- Banco de dados: `sucurihc_ctf`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `evento`
+-- Estrutura para tabela `evento`
 --
 
 CREATE TABLE `evento` (
@@ -32,20 +32,21 @@ CREATE TABLE `evento` (
   `inicio` datetime NOT NULL,
   `formatoflag` varchar(255) COLLATE latin1_bin NOT NULL,
   `link_evento` varchar(255) COLLATE latin1_bin NOT NULL,
-  `link_score` varchar(255) COLLATE latin1_bin NOT NULL
+  `link_score` varchar(255) COLLATE latin1_bin NOT NULL,
+  `link_team` varchar(255) COLLATE latin1_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `flag`
+-- Estrutura para tabela `flag`
 --
 
 CREATE TABLE `flag` (
   `idflag` int(11) NOT NULL,
   `titulo` varchar(255) NOT NULL,
   `tipo` varchar(200) NOT NULL,
-  `descricao` varchar(400) NOT NULL,
+  `descricao` varchar(800) NOT NULL,
   `resposta` varchar(200) NOT NULL,
   `evento` int(11) NOT NULL,
   `valor` int(14) NOT NULL
@@ -54,7 +55,20 @@ CREATE TABLE `flag` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `recuperacao`
+-- Estrutura para tabela `log`
+--
+
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `ip` varchar(20) NOT NULL,
+  `acesso` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `recuperacao`
 --
 
 CREATE TABLE `recuperacao` (
@@ -65,13 +79,14 @@ CREATE TABLE `recuperacao` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `resolvidas`
+-- Estrutura para tabela `resolvidas`
 --
 
 CREATE TABLE `resolvidas` (
   `id` int(11) NOT NULL,
   `flagid` int(15) NOT NULL,
   `userid` int(15) NOT NULL,
+  `idteam` int(11) NOT NULL,
   `valor` int(11) NOT NULL,
   `evento` varchar(255) NOT NULL DEFAULT '',
   `dhresposta` datetime NOT NULL
@@ -80,7 +95,7 @@ CREATE TABLE `resolvidas` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `score`
+-- Estrutura para tabela `score`
 --
 
 CREATE TABLE `score` (
@@ -91,23 +106,24 @@ CREATE TABLE `score` (
   `evento` varchar(255) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- -------------------------------------------------------
+-- --------------------------------------------------------
 
 --
--- Estrutura da tabela `recuperacao`
+-- Estrutura para tabela `scoreteam`
 --
 
-CREATE TABLE recuperacao (
-  utilizador  VARCHAR(255) NOT NULL,
-  confirmacao VARCHAR(40) NOT NULL,
-  KEY(utilizador, confirmacao)
-)
-
+CREATE TABLE `scoreteam` (
+  `id_score` int(11) NOT NULL,
+  `score` bigint(20) NOT NULL,
+  `idteam` int(15) NOT NULL,
+  `time` datetime NOT NULL,
+  `evento` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sr_usuarios_secret`
+-- Estrutura para tabela `sr_usuarios_secret`
 --
 
 CREATE TABLE `sr_usuarios_secret` (
@@ -122,7 +138,7 @@ CREATE TABLE `sr_usuarios_secret` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `team`
+-- Estrutura para tabela `team`
 --
 
 CREATE TABLE `team` (
@@ -133,45 +149,58 @@ CREATE TABLE `team` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
 
 --
--- Indexes for dumped tables
+-- Índices de tabelas apagadas
 --
 
 --
--- Indexes for table `evento`
+-- Índices de tabela `evento`
 --
 ALTER TABLE `evento`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `nome` (`nome`);
 
 --
--- Indexes for table `flag`
+-- Índices de tabela `flag`
 --
 ALTER TABLE `flag`
   ADD PRIMARY KEY (`idflag`),
   ADD KEY `evento_fk_idx` (`evento`);
 
 --
--- Indexes for table `recuperacao`
+-- Índices de tabela `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `recuperacao`
 --
 ALTER TABLE `recuperacao`
   ADD KEY `utilizador` (`utilizador`,`confirmacao`);
 
 --
--- Indexes for table `resolvidas`
+-- Índices de tabela `resolvidas`
 --
 ALTER TABLE `resolvidas`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `flagid` (`flagid`,`userid`);
 
 --
--- Indexes for table `score`
+-- Índices de tabela `score`
 --
 ALTER TABLE `score`
   ADD PRIMARY KEY (`id_score`),
   ADD UNIQUE KEY `iduser` (`iduser`,`evento`);
 
 --
--- Indexes for table `sr_usuarios_secret`
+-- Índices de tabela `scoreteam`
+--
+ALTER TABLE `scoreteam`
+  ADD PRIMARY KEY (`id_score`),
+  ADD UNIQUE KEY `iduser` (`idteam`,`evento`);
+
+--
+-- Índices de tabela `sr_usuarios_secret`
 --
 ALTER TABLE `sr_usuarios_secret`
   ADD PRIMARY KEY (`Id`),
@@ -180,7 +209,7 @@ ALTER TABLE `sr_usuarios_secret`
   ADD KEY `idteam_idx` (`idteam`);
 
 --
--- Indexes for table `team`
+-- Índices de tabela `team`
 --
 ALTER TABLE `team`
   ADD PRIMARY KEY (`idteam`),
@@ -189,57 +218,67 @@ ALTER TABLE `team`
   ADD KEY `adm_fk_idx` (`adm`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas apagadas
 --
 
 --
--- AUTO_INCREMENT for table `evento`
+-- AUTO_INCREMENT de tabela `evento`
 --
 ALTER TABLE `evento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
--- AUTO_INCREMENT for table `flag`
+-- AUTO_INCREMENT de tabela `flag`
 --
 ALTER TABLE `flag`
-  MODIFY `idflag` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `idflag` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 --
--- AUTO_INCREMENT for table `resolvidas`
+-- AUTO_INCREMENT de tabela `log`
+--
+ALTER TABLE `log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1758;
+--
+-- AUTO_INCREMENT de tabela `resolvidas`
 --
 ALTER TABLE `resolvidas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2787;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5137;
 --
--- AUTO_INCREMENT for table `score`
+-- AUTO_INCREMENT de tabela `score`
 --
 ALTER TABLE `score`
-  MODIFY `id_score` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21371;
+  MODIFY `id_score` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40909;
 --
--- AUTO_INCREMENT for table `sr_usuarios_secret`
+-- AUTO_INCREMENT de tabela `scoreteam`
+--
+ALTER TABLE `scoreteam`
+  MODIFY `id_score` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3717;
+--
+-- AUTO_INCREMENT de tabela `sr_usuarios_secret`
 --
 ALTER TABLE `sr_usuarios_secret`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=422;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=723;
 --
--- AUTO_INCREMENT for table `team`
+-- AUTO_INCREMENT de tabela `team`
 --
 ALTER TABLE `team`
-  MODIFY `idteam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `idteam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
 --
--- Constraints for dumped tables
+-- Restrições para dumps de tabelas
 --
 
 --
--- Constraints for table `flag`
+-- Restrições para tabelas `flag`
 --
 ALTER TABLE `flag`
   ADD CONSTRAINT `evento_fk` FOREIGN KEY (`evento`) REFERENCES `evento` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `sr_usuarios_secret`
+-- Restrições para tabelas `sr_usuarios_secret`
 --
 ALTER TABLE `sr_usuarios_secret`
   ADD CONSTRAINT `idteam` FOREIGN KEY (`idteam`) REFERENCES `team` (`idteam`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `team`
+-- Restrições para tabelas `team`
 --
 ALTER TABLE `team`
   ADD CONSTRAINT `adm_fk` FOREIGN KEY (`adm`) REFERENCES `sr_usuarios_secret` (`Id`) ON DELETE SET NULL ON UPDATE NO ACTION;
